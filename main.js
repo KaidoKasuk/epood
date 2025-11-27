438be13f37fbdbf2db578f7e477cb2f7500337c5
kasuk <student@Administrators-Mac-mini-2.local>
2025-11-27 16:55:21 +0200

    3 ül poolik

--- main.js
+++ main.js
@@ -12,8 +12,57 @@ class Product {
     return (price - price * (discount / 100)).toFixed(2);
   }
 }
-
+// 1 toodete lisamine e-poodi //
 laptop = new Product(1, "sülearvuti", 999.99, "elektroonika");
-console.log(laptop.describe());
+phone = new Product(2, "luts", 116.99, "elektroonika");
 
+console.log(laptop.describe());
+//allahindlus
 console.log(Product.discountedPrice(laptop.price, 10)); // 10% allahindlus
+
+// 2 ostukorv //
+
+class Cart {
+  constructor() {
+    this.items = [];
+  }
+
+  addProduct(product, quantity) {
+    this.items.push({ product, quantity });
+  }
+  removeProduct(productId) {
+    this.items = this.items.filter((item) => item.product.id !== productId);
+  }
+  calculateTotal() {
+    return this.items
+      .reduce((sum, item) => sum + item.product.price * item.quantity, 0)
+      .toFixed(2);
+  }
+
+  get totalItems() {
+    return this.items.reduce((sum, item) => sum + item.quantity, 0);
+  }
+}
+
+const cart = new Cart();
+//lisan produkte
+cart.addProduct(laptop, 3);
+cart.addProduct(phone, 0);
+console.log(cart.items); // array
+console.log(cart.calculateTotal()); // Kokku hind
+
+console.log(cart.totalItems); //kokku asju
+
+// 3 tellimused ja kliendi andmed (appi) //
+
+class Order {
+  constructor() {
+    this.items = [];
+  }
+  orderDate(date) {
+    return this.items.push({ date });
+  }
+}
+const date = new Date(1);
+const order = new Order(cart);
+console.log(order.items); // array
